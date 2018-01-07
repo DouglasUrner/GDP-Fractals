63b3180714a98340f54757529bdaf638ca30947b
Douglas Urner <dlu@canishe.com>
2018-01-06 20:42:40 -0800

    Initial (late) checkin.

---
+++ Constructing A Fractal/Assets/Fractal.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour {
+
+	public Mesh mesh;
+	public Material material;
+	public int maxDepth;
+
+	private int depth;
+
+	private Material[] materials;
+
+	private void InitializeMaterials() {
+		materials = new Material[maxDepth + 1];
+		for (int i = 1; i <= maxDepth; i++) {
+			materials[i] = new Material(material);
+			materials[i].color = Color.Lerp(Color.white, Color.yellow, (float)i / maxDepth);
+		}
+	}
+
+	private void Start() {
+		if (materials == null) {
+			InitializeMaterials();
+		}
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+		GetComponent<MeshRenderer>().material = materials[depth];
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
+		}
+	}
+
+	private static Vector3[] childDirections = {
+		Vector3.up,
+		Vector3.right,
+		Vector3.left,
+		Vector3.forward,
+		Vector3.back
+	};
+
+	private static Quaternion[] childOrientations = {
+		Quaternion.identity,
+		Quaternion.Euler(0f, 0f, -90f),
+		Quaternion.Euler(0f, 0f, 90f),
+		Quaternion.Euler(90f, 0f, 0f),
+		Quaternion.Euler(-90f, 0f, 0f)
+	};
+
+	public float minYieldSecs = 0.1f;
+	public float maxYieldSecs = 0.5f;
+
+	private IEnumerator CreateChildren() {
+		for (int i = 0; i < childDirections.Length; i++) {
+			yield return new WaitForSeconds(Random.Range(minYieldSecs, maxYieldSecs));
+			new GameObject("FractalChild").AddComponent<Fractal>().
+				Initialize(this, i);
+		}
+	}
+
+	public float childScale;
+
+	private void Initialize(Fractal parent, int childIndex) {
+		mesh = parent.mesh;
+		materials = parent.materials;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		childScale = parent.childScale;
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition =
+			childDirections[childIndex] * (0.5f + 0.5f * childScale);
+		transform.localRotation = childOrientations[childIndex];
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
